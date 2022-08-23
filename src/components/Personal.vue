1a2fcdd59cbaad9fcea24c2bcd35ab586af79452
FarhanXIRPL2 <farhanrian321@gmail.com>
2022-08-23 19:51:31 +0700

    cv project

---
+++ src/components/Personal.vue
@@ -0,0 +1,12 @@
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/foto-cv.jpeg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="Farhan Rian Hidayat" />
+
+      <nav>
+        <RouterLink to="/">About Me</RouterLink>
+        <RouterLink to="/about">Skill</RouterLink>
+      </nav>
+    </div>
+  </header>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,77 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Personal Profile</template>
+    <p>I'm a software engineering student. I beginner web developer with basic web skills, Always want to learn and improve</p>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Education</template>
+    
+    <p>
+      2020-2023
+    </p>
+    <a href="https://smktarunabhakti.net/" target="_blank">SMK Taruna Bhakti</a>
+    <p>
+      Web Developer
+    </p>
+
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Experience</template>
+
+    <ul>
+      <li>Make Hotel Booking Design</li>
+      <li>Make Food Delivery Design</li>
+      <li>Make Sarpras</li>
+      <li>Make E-commerce Design</li>
+    </ul>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Language</template>
+    <ul>
+      <li>Indonesia</li>
+      <li>Bahasa</li>
+    </ul>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Contact</template>
+    <ul>
+      <li>
+      (+62) 813-8127-2831
+      </li>
+      <li>
+      farhanrian321@gmail.com
+      </li>
+      <li>
+        <a target="_blank" href="https://github.com/FarhanRianHidayat">github.com/FarhanRianHidayat</a>
+      </li>
+    </ul>
+  </WelcomeItem>
+</template>
